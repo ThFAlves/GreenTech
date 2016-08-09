38d3ea060d69caf071a70bdecca57658cdcc0b24
HyagoHirai <Hyago1@MacBook-Pro-de-Hyago.local>
2016-08-09 15:46:19 -0300

    commit inicial

---
+++ GreenTech/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GreenTech
+//
+//  Created by HyagoHirai on 09/08/16.
+//  Copyright © 2016 HyagoHirai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
